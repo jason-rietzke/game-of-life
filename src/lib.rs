e8cd9aa44eb5f49e487f5c4a4a0f9e190082c04b
Jason Rietzke <jason.rietzke@livereader.com>
2023-07-16 04:11:50 +0200

    render universe into a html canvas

--- src/lib.rs
+++ src/lib.rs
@@ -20,17 +20,15 @@ pub struct Universe {
 
 #[wasm_bindgen]
 impl Universe {
-    pub fn new() -> Universe {
-        let width = 64;
-        let height = 64;
+    pub fn new(width: u32, height: u32) -> Universe {
+        utils::set_panic_hook();
+        let width = width;
+        let height = height;
 
         let cells = (0..width * height)
-            .map(|i| {
-                if i % 2 == 0 || i % 7 == 0 {
-                    Cell::Alive
-                } else {
-                    Cell::Dead
-                }
+            .map(|_| match js_sys::Math::random() {
+                x if x < 0.5 => Cell::Alive,
+                _ => Cell::Dead,
             })
             .collect();
 
@@ -39,6 +37,16 @@ impl Universe {
             height,
             cells,
         }
+    }
+
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+    pub fn cells(&self) -> *const Cell {
+        self.cells.as_ptr()
     }
 
     pub fn render(&self) -> String {
--- www/src/main.ts
+++ www/src/main.ts
@@ -1,15 +1,77 @@
 import "./style.css";
-import { Universe } from "game-of-life";
+// @ts-ignore
+import { memory } from "game-of-life/game_of_life_bg.wasm";
+import { Universe, Cell } from "game-of-life";
 
-const canvas = document.getElementById("game-of-life-canvas");
+const CELL_SIZE = 3; // px
+const GRID_COLOR = "#393a3c";
+const DEAD_COLOR = "#151618";
+const ALIVE_COLOR = "#94a8ff";
+
+const universe = Universe.new(window.innerWidth / CELL_SIZE, window.innerHeight / CELL_SIZE);
+const width = universe.width();
+const height = universe.height();
+
+const canvas = document.getElementById("game-of-life-canvas") as HTMLCanvasElement | null;
 if (!canvas) throw new Error("Canvas not found");
+canvas.height = (CELL_SIZE + 1) * height + 1;
+canvas.width = (CELL_SIZE + 1) * width + 1;
 
-const universe = Universe.new();
+const ctxType = "2d";
+const ctx = canvas.getContext(ctxType);
+if (!ctx) throw new Error(`Context ${ctxType} not found`);
 
 const renderLoop = () => {
-	canvas.textContent = universe.render();
 	universe.tick();
+
+	drawGrid();
+	drawCells();
 
 	requestAnimationFrame(renderLoop);
 };
+
+const drawGrid = () => {
+	ctx.beginPath();
+	ctx.strokeStyle = GRID_COLOR;
+
+	// Vertical lines.
+	for (let i = 0; i <= width; i++) {
+		ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+		ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
+	}
+
+	// Horizontal lines.
+	for (let j = 0; j <= height; j++) {
+		ctx.moveTo(0, j * (CELL_SIZE + 1) + 1);
+		ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
+	}
+
+	ctx.stroke();
+};
+
+const drawCells = () => {
+	const cellsPtr = universe.cells();
+	const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
+
+	ctx.beginPath();
+
+	for (let row = 0; row < height; row++) {
+		for (let col = 0; col < width; col++) {
+			const idx = getIndex(row, col);
+
+			ctx.fillStyle = cells[idx] === Cell.Dead ? DEAD_COLOR : ALIVE_COLOR;
+
+			ctx.fillRect(col * (CELL_SIZE + 1) + 1, row * (CELL_SIZE + 1) + 1, CELL_SIZE, CELL_SIZE);
+		}
+	}
+
+	ctx.stroke();
+};
+
+const getIndex = (row: number, column: number) => {
+	return row * width + column;
+};
+
+drawGrid();
+drawCells();
 requestAnimationFrame(renderLoop);
